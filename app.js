ea2c93eb007a463e6e07a9ad1e5dc91cc13631ea
{ giorgi } <giorgi.zautashvili@me.com>
2016-09-26 13:38:37 +0200

    add ng-mode and things related to event loop

--- app.js
+++ app.js
@@ -7,9 +7,17 @@
     var mainController = function ($scope) {
 
         $scope.name = 'Giorgi';
+        
+            setTimeout(function() {
+
+            $scope.$apply(function() {
+
+                $scope.twitterHandle = 'giorgi_pro'
+            });
+
+        }, 3000);
     }
-    
+        
     app.controller('mainController', ['$scope', mainController]);
     
 }());
-
